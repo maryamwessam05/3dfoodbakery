f789e6b3dcd8d717c12f080066302163e3c20e70
maryamwessam05 <23-101015@students.eui.edu.eg>
2025-11-23 23:00:22 +0200

    added contact

---
+++ contactcode.js
@@ -0,0 +1,10 @@
+       let burger = document.querySelector(".burger");
+        let menu = document.querySelector(".burgermenu");
+        let closeX = document.getElementById("x");
+burger.onclick = () => {
+    menu.classList.add("active");
+  };
+  
+  closeX.onclick = () => {
+    menu.classList.remove("active");
+  };
