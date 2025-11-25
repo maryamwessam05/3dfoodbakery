d6ec22e50195c7f82682dff94aed5c35a7d338ff
maryamwessam05 <23-101015@students.eui.edu.eg>
2025-11-25 23:29:51 +0200

    finished menu

---
+++ menu.js
@@ -0,0 +1,38 @@
+       let burger = document.querySelector(".burger");
+        let menu = document.querySelector(".burgermenu");
+        let closeX = document.getElementById("x");
+burger.onclick = () => {
+    menu.classList.add("active");
+  };
+  
+  closeX.onclick = () => {
+    menu.classList.remove("active");
+  };
+
+
+  function increase(numid){
+    let num = document.getElementById(numid).innerHTML;
+
+    document.getElementById(numid).innerHTML = ++num;
+  }
+
+    function decrease(numid){
+    let num = document.getElementById(numid).innerHTML;
+    if(num != 0){
+
+        document.getElementById(numid).innerHTML = --num;
+    }
+  }
+
+  function crois(){
+    document.querySelector(".croisgame").style.opacity="1";
+  }
+    function cup(){
+    document.querySelector(".cupgame").style.opacity="1";
+  }
+    function don(){
+    document.querySelector(".donutgame").style.opacity="1";
+  }
+    function pret(){
+    document.querySelector(".pretzelgame").style.opacity="1";
+  }
